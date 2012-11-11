a8eec242e111949636bb48f22d78dda48b2daf69
takezoe <takezoe@gmail.com>
2012-11-11 17:44:24 +0900

    Revert "Remove CharacterEncodingFilter because it's provided by Tomcat."
    This reverts commit 4b59ca366c109b3730d78f4a9ec41bd7b461a2c6.

---
+++ src/main/scala/jp/sf/amateras/play2/httpsession/CharacterEncodingFilter.scala
@@ -0,0 +1,31 @@
+package jp.sf.amateras.play2.httpsession
+
+import javax.servlet.http._
+import javax.servlet.http.{HttpSession => ServletSession}
+import javax.servlet._
+import javax.servlet.annotation._
+
+/**
+ * Sets the request character encoding through ServletRequest#setCharacterEncoding().
+ * By default, this filter set "UTF-8". You can also configure this encoding in web.xml.
+ */
+@WebFilter(urlPatterns = Array("/*"), asyncSupported = true)
+class CharacterEncodingFilter extends Filter {
+
+  var encoding: String = null
+
+  def init(config: FilterConfig): Unit = {
+    encoding = config.getInitParameter("requestEncoding") match {
+      case null => "UTF-8"
+      case s    => s
+    }
+  }
+
+  def destroy(): Unit = {}
+
+  def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
+    request.setCharacterEncoding(encoding)
+    chain.doFilter(request, response)
+  }
+
+}
